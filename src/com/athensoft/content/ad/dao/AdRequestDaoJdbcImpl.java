6119f06e4de746df81f043384532d153596f47cc
athens <athens314@hotmail.com>
2018-07-04 20:37:54 -0400

    changed ad request dao

--- src/com/athensoft/content/ad/dao/AdRequestDaoJdbcImpl.java
+++ src/com/athensoft/content/ad/dao/AdRequestDaoJdbcImpl.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.athensoft.content.ad.entity.AdRequest;
